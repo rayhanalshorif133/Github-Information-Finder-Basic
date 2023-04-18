2d60098c4e1308c83bc098db9713533e5c305e70
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-18 14:47:00 +0600

    info and goto github

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
-axios.get("https://api.github.com/search/users?q=rayhanalshorif132")
-.then((response) => {
-    var data = response.data.items[0];
-    data = data == undefined ? "No data found" : data;
+// axios.get("https://api.github.com/search/users?q=rayhanalshorif132")
+// .then((response) => {
+//     var data = response.data.items[0];
+//     data = data == undefined ? "No data found" : data;
+//     console.log(data);
+// });
+
+async function getData(url) {
+    const response = await axios.get(url);
+    const data = response.data.items[0];
     console.log(data);
+    $(".avatar").attr("src", data.avatar_url);
+    $(".name").text(data.login);
+    $(".bio").text(data.bio);
+    $(".followers").text(data.followers);
+    $(".goto_github").attr("href", data.html_url);
+}
+
+$(function (){
+    $("#username").on("keyup", function (){
+        var username = $(this).val();
+        if(username.length > 2){
+            let url = "https://api.github.com/search/users?q="+username;
+            setTimeout(() => {
+                getData(url);
+            }, 3000);
+        }
+    });
+
+    handleGotoGithub();
 });
+
+handleGotoGithub = () =>{
+    $(".goto_github").hover(function () {
+        let html = `Github <i class="fa-solid fa-angles-right"></i>`
+        $(this).find(".btn").html(html);
+    });
+
+    $(".goto_github").mouseleave(function () {
+        let html = `<i class="fab fa-github"></i> Github`
+        $(this).find(".btn").html(html);
+    });
+}
